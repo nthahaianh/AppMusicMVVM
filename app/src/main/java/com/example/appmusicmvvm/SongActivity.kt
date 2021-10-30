2422b58b550b886309eaa6e9d3a0509b91172207
nthahaianh <ntha.haianh@gmail.com>
2021-10-30 18:29:10 +0700

    commit-create

---
+++ app/src/main/java/com/example/appmusicmvvm/SongActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appmusicmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SongActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song)
+    }
+}
